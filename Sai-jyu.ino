252e80541fe6b3a16fb74a6cf365ecf5a164b260
yosan443 <73591068+yosan443@users.noreply.github.com>
2022-07-02 08:32:54 +0000

    無接続ピンの不安な電圧を利用した擬似ランダム試作版

--- Sai-jyu.ino
+++ Sai-jyu.ino
@@ -1,35 +1,55 @@
+int a = 0;
 int x = 0;
 int han = 70;
-long mato1;
 
 void setup()
 {
+    pinMode(0, OUTPUT);
     pinMode(1, OUTPUT);
     pinMode(2, OUTPUT);
     pinMode(3, OUTPUT);
     pinMode(4, OUTPUT);
     pinMode(5, OUTPUT);
+    pinMode(6, OUTPUT);
+    pinMode(7, OUTPUT);
+    pinMode(8, OUTPUT);
+    pinMode(9, OUTPUT);
 }
 
 void loop()
 {
-    mato1 = random(3);
-    if (mato1 = 0)
+    x = analogRead(6);
+    a = (x % 10);
+    if (a = 0)
     {
         analogWrite(1, han);
-    }else if (mato1 = 1)
+    }else if (a = 1)
     {
         analogWrite(2, han);
-    }else if (mato1 = 2)
+    }else if (a = 2)
     {
         analogWrite(3, han);
-    }else if (mato1 = 3)
+    }else if (a = 3)
     {
         analogWrite(4, han);
-    }else if (mato1 = 4)
+    }else if (a = 4)
     {
         analogWrite(5, han);
+    }else if (a = 5)
+    {
+        analogWrite(6, han);
+    }else if (a = 6)
+    {
+        analogWrite(7, han);
+    }else if (a = 7)
+    {
+        analogWrite(8, han);
+    }else if (a = 8)
+    {
+        analogWrite(9, han);
+    }else if (a = 9)
+    {
+        analogWrite(0, han);
     }
-    delay(1500);
-    }
+    
 }
